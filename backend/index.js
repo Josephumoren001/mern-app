a6cb2c35aa99f322a9fbd7653f218ad432862b86
Josephumoren001 <josephumoren017@gmail.com>
2024-09-26 00:51:02 +0100

    cors remove

--- backend/index.js
+++ backend/index.js
@@ -30,13 +30,18 @@ class Server {
       crossOriginEmbedderPolicy: false,
     }));
     
-    this.app.use(cors({
-      origin: ['http://localhost:8080', 'https://deskstones.com', 'https://www.deskstones.com'],
+    // this.app.use(cors({
+    //   origin:  ['http://localhost:8080', 'https://deskstones.com', 'https://www.deskstones.com'],
+    //   credentials: true,
+    // }));
+    app.use(cors({
+      origin: '*', // Accepts requests from all origins
       credentials: true,
     }));
     
+    
     this.app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'https://www.deskstones.com');
+      res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
       res.header('Access-Control-Allow-Credentials', 'true');

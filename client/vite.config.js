c7d8d358613ca10c98b3da03435ab09440ac35ff
Josephumoren001 <josephumoren017@gmail.com>
2024-09-25 21:52:18 +0100

    pack update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://deskstones-a.onrender.com',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: true
       },
